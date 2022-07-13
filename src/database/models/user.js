568a86d6af38df46c0215a4bff3681312916ac8d
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-07-13 17:03:56 -0300

    feat: inclui model User

---
+++ src/database/models/user.js
@@ -0,0 +1,19 @@
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+  User.associate = (db) => {
+    User.hasMany(db.Pet, { as: 'pets', foreignKey: 'userId' });
+  }
+
+  return User;
+};
+
+module.exports = createUser;
