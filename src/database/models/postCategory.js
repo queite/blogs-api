39c40688c490a6f3605c07596d8f6f99a1e6d86b
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-07-14 19:13:09 -0300

    feat: inclui model PostCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,36 @@
+const createPostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  }, {
+    tableName: 'PostCategories',
+    timestamps: false,
+  });
+
+  PostCategory.associate = (db) => {
+    db.Category.belongsToMany(db.BlogPost, {
+      as: 'posts',
+      through: 'PostCategory',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    db.BlogPost.belongsToMany(db.Category, {
+      as: 'posts',
+      through: 'PostCategory',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  }
+
+  return PostCategory;
+};
+
+module.exports = createPostCategory;
